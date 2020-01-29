2eaeda83a358511c7a273cb0cdf0036bb6768241
Marco Zingoni <marco.zingoni@gmail.com>
2020-01-29 15:01:54 +0100

    Input e twoWayDataBinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ import { Component } from "@angular/core";
 export class AppComponent {
   title: string = "Hello World";
 
+  moduleVar: string;
+  isPalindrom = false;
+
   primoNumero: number = 4;
   secondoNumero: number = 6;
 
@@ -27,5 +30,24 @@ export class AppComponent {
 
   assegnaNumeroAVariabile(): void {
     this.variabileNumericaDaAssegnare = this.secondoNumero * this.secondoNumero;
+  }
+
+  elaboraVariabileInput(): void {
+    let reversed = this.ribaltaStringa(this.moduleVar);
+    this.isPalindrom = false;
+    if (reversed == this.moduleVar) {
+      this.isPalindrom = true;
+    }
+  }
+
+  resettaPalindrom(): void {
+    this.isPalindrom = false;
+  }
+
+  ribaltaStringa(str: string): string {
+    return str
+      .split("")
+      .reverse()
+      .join("");
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { FirstControllerComponent } from './controllers/first-controller/first-controller.component';
-import { SecondControllerComponent } from './controllers/second-controller/second-controller.component';
-import { LibriComponent } from './controllers/libri/libri.component';
-import { LibroComponent } from './controllers/libro/libro.component';
+import { FirstControllerComponent } from "./controllers/first-controller/first-controller.component";
+import { SecondControllerComponent } from "./controllers/second-controller/second-controller.component";
+import { LibriComponent } from "./controllers/libri/libri.component";
+import { LibroComponent } from "./controllers/libro/libro.component";
 
 @NgModule({
-  declarations: [AppComponent, FirstControllerComponent, SecondControllerComponent, LibriComponent, LibroComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    FirstControllerComponent,
+    SecondControllerComponent,
+    LibriComponent,
+    LibroComponent
+  ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
